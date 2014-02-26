169a7d3ed1e1dbc0bed49c6ef3ba35220d1121c8
Lerikio <ssilfir@hotmail.fr>
2014-02-26 15:44:38 +0100

    Ajout des vues de listes

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+# -*- coding: utf-8 -*-
+
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
+
+from kivy.uix.label import Label
+from kivy.animation import Animation
+
 
 from kivy.config import Config
 Config.set('graphics', 'width', '400')
@@ -15,30 +21,81 @@ class Connexion(Screen):
 		self.ids['host'].text = ""
 		self.ids['port'].text = ""
 
-class Connected(Screen):
+class MainMenu(Screen):
 	def disconnect(self):
 		self.manager.transition = SlideTransition(direction="right")
 		self.manager.current = 'connexion'
 		self.manager.get_screen('connexion').resetForm()
 
-class ConnectedApp(App):
-	def build(self):
-		screen = Connected()
-		screen.name = 'connected'
-		return screen
+	def go_to(self, list):
+		self.manager.transition = SlideTransition(direction="left")
+		self.manager.current = 'lists'
+		self.manager.get_screen('lists').load(list)
+
+class SubView(Screen):
+	def back_to_main_menu(self):
+		self.manager.transition = SlideTransition(direction="right")
+		self.manager.current = 'menu'
+
+
+class ListScreen(SubView):
+	def load(self, list):
+		# Récupérer la liste correspondante à l'adresse host:port/list
+		pass
+
+class ShowScreen(SubView):
+	def load(self, object):
+		# Récupérer les infos de l'objet à visualiser
+		pass
+
+
+class DraggableLabel(Label):
+    '''A label you can drag upside-down'''
+    def on_touch_down(self, touch):
+        if self.collide_point(*touch.pos):
+            # assure ourselves we will get the updates of this motion
+            touch.grab(self)
+            return True
+
+        return super(DraggableLabel, self).on_touch_down(touch)
+
+    def on_touch_move(self, touch):
+        if touch.grab_current is self:
+            # really straightforward...
+            self.y = touch.y
+            return True
+
+        return super(DraggableLabel, self).on_touch_move(touch)
+
+    def on_touch_up(self, touch):
+        if touch.grab_current is self:
+            # check if the movement direction was up or down
+            if touch.dy < 0:
+                a = Animation(y=0) # down? put the bar all the way down
+            else:
+                a = Animation(top=self.parent.top) # up? put it at the top
+
+            a.start(self) # actually start the animation
+            return True
+
+        return super(DraggableLabel, self).on_touch_up(touch)
+
+    def restart(self):
+    	Animation(top=self.parent.top).start(self)
+
 
 class InsightApp(App):
 	def build(self):
 		manager = ScreenManager()
 
-		# ajout de l'instance de login
+		# ajout de l'instance de connexion
 		manager.add_widget(Connexion(name='connexion'))
 
-		# ajout de la vue 'connected'
-		app = ConnectedApp()
-		app.load_kv()
-		connectedView = app.build()
-		manager.add_widget(connectedView)
+		# ajout de la vue 'menu'
+		manager.add_widget(MainMenu(name='menu'))
+
+		# ajout de la vue 'lists'
+		manager.add_widget(ListScreen(name='lists'))
 
 		manager.transition = SlideTransition(direction="left")
 		return manager
